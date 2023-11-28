beacb70ad6da23787c2da1190627897cec926706
jaeger187 <133166571+jaeger187@users.noreply.github.com>
2023-11-28 10:08:59 +0100

    Add files via upload

---
+++ Stundenplanner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stundenplanner
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Hallo Welt");
+            Console.WriteLine("Test");
+            Console.WriteLine("Online");
+            Console.WriteLine("hosasdl");
+            Console.WriteLine("sasd");
+            Console.WriteLine("Online")
+            Console.WriteLine( "Stefan");
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
