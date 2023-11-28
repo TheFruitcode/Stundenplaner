2ce09e16e72c7de4f3386335d779f6e7b522f798
Stefan216 <Stefan.Kostic567@gmail.com>
2023-11-28 10:48:07 +0100

    button 3 grossgezogen

--- Stundenplanner/Form1.Designer.cs
+++ Stundenplanner/Form1.Designer.cs
@@ -53,7 +53,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(48, 367);
+            this.button1.Location = new System.Drawing.Point(81, 348);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -63,7 +63,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(209, 367);
+            this.button2.Location = new System.Drawing.Point(184, 348);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -73,9 +73,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(128, 416);
+            this.button3.Location = new System.Drawing.Point(81, 391);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(178, 26);
             this.button3.TabIndex = 4;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
