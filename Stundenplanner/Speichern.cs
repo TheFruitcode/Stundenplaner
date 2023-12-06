b06625190d0d0f14ee7b6f67d68727ae53c415ed
TheFruitcode <falco.rensner@gmx.de>
2023-12-06 10:46:50 +0100

    Speichern aktualisiert und Termin Klasse hinzugeügt

--- Stundenplanner/Speichern.cs
+++ Stundenplanner/Speichern.cs
@@ -1,25 +1,33 @@
-﻿using View.cs;
-using System;
-using System.Windows.Forms;
-using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-public class Speichern
+
+namespace WindowsFormsApp1
 {
-    calender = monthCalendar1_DateChanged;
-    beschreibung = richTextBox1_TextChanged;
+    public class Speichern
+    {
+        Console.WriteLine("Hello World");
 
-    public Speichern()
-	{
-        string myConnectionString = "server=127.0.0.1; uid=root; pwd=''; database:stundenplaner;";
+        string calender = monthCalendar1;
+        string Terminbeschreibung = richTextBox1;
+        public Speichern()
+        {
+            string myConnectionString = "server=127.0.0.1; uid=root; pwd=''; database:stundenplaner;";
 
-        MySqlConnection conn = new MySqlConnection(myConnectionString);
-        conn.Open();
+            MySqlConnection conn = new MySqlConnection(myConnectionString);
+            conn.Open();
 
-        MySqlCommand mycommand = conn.CreateCommand();
+            MySqlCommand mycommand = conn.CreateCommand();
 
-        mycommand.CommandText = "Insert into TerminSpeichern (ID_Termine, Datum, Beschreibung) values();";
-        conn.Close();
+            mycommand.CommandText = "Insert into TerminSpeichern (ID_Termine, Datum, Beschreibung) values();";
+            conn.Close();
+        }
     }
 }
